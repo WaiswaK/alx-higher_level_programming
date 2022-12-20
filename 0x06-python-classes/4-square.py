7b05be9fff0891d2c93523d6c2a455ccbbac172d
Kenneth Waiswa <wswkenneth7@hotmail.com>
2022-12-20 10:59:38 -0800

    Mandatory Tasks 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,34 +1,30 @@
 #!/usr/bin/python3
-
-"""Square Class.
-This module contains a class that defines a square.
-Usage Example:
-    Square = __import__('4-square').Square
-    my_square = Square(3)
-    print(type(my_square))
-    print(my_square.__dict__)
-"""
+"""Define a class Square."""
 
 
 class Square:
-    """Defines the blueprint of a square.
-    Attribute:
-        size: An integer indicating the size of the square object.
-    """
+    """Represent a square."""
 
     def __init__(self, size=0):
-        """An object constructor method.
-        Initiatilizes Square with size.
-        Arg:
-            size: A integer representing object size.
-                  Has a default value of 0.
+        """Initialize a new square.
+        Args:
+            size (int): The size of the new square.
         """
-        self.__size = size
+        self.size = size
 
     @property
     def size(self):
-        """Gets the size private attribute value.
-        Returns:
-            The size private attribute
-        """
-        return self.__size
+        """Get/set the current size of the square."""
+        return (self.__size)
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """Return the current area of the square."""
+        return (self.__size * self.__size)
