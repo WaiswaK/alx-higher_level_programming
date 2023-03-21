762ec6b861797e3383021b5cb35aaa7793ca14e7
Kenneth Waiswa <wswkenneth7@hotmail.com>
2023-03-21 05:39:23 -0700

    Fix

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,4 +1,4 @@
-i#!/usr/bin/python3
+#!/usr/bin/python3
 """List all states from a given db sorted in ascending order by id
 Username, password, and database names are given as user args
 """
@@ -20,4 +20,3 @@ if __name__ == "__main__":
 
     cur.close()
     db.close()
-
